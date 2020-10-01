035bcdd49681ae5920aff3f63c6e91144f5a5f6c
Hoagie <hoagie@different.hu>
2020-10-01 09:53:32 +0200

    it_should_have_required_fields improve

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,10 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/)
 and this project adheres to [Semantic Versioning](http://semver.org/).
 
+## [0.10.25] - 2020-09-28
+###changed
+-it_should_have_required_fields improve
+
 ## [0.10.24] - 2020-09-28
 ###added
 -Test utilities
--- src/app/Traits/DwfwTestCase.php
+++ src/app/Traits/DwfwTestCase.php
@@ -9,14 +9,21 @@ use Exception;
 
 trait DwfwTestCase
 {
-    protected function it_should_have_required_fields($api_url, $required_fields, $api_data)
+    protected function it_should_have_required_fields($api_url, $required_fields, $api_data, $token = null)
     {
         foreach ($required_fields as $required_field) {
             if (!isset($api_data[$required_field])) {
                 throw new Exception('Unset required field in api data');
             }
             $response = $this
-                ->postJson($api_url, Arr::except($api_data, [$required_field]))
+                ->postJson(
+                    $api_url,
+                    Arr::except($api_data, [$required_field]),
+                    [
+                        'Accept' => 'application/json',
+                        'Authorization' => $token ? 'Bearer ' . $token : '',
+                    ]
+                )
                 ->decodeResponseJson();
             $this->assertTrue($response['error']);
             $this->assertArrayHasKey($required_field, $response['message']);

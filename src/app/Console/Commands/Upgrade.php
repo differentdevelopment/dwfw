8ecfb9511745c398bfd1e33309e22c6c78cc0a35
Urudin <kirtap9408@gmail.com>
2021-02-04 12:16:43 +0100

    Version update

--- src/app/Console/Commands/Upgrade.php
+++ src/app/Console/Commands/Upgrade.php
@@ -12,7 +12,7 @@ class Upgrade extends Command
 
     use PrettyCommandOutput;
 
-    const VERSION = '0.14.0';
+    const VERSION = '0.14.1';
     protected string $finish_message;
     /**
      * Array of methods used for upgrading to the given version
--- src/config/dwfw.php
+++ src/config/dwfw.php
@@ -1,5 +1,5 @@
 <?php
 
 return [
-    'version' => '0.14.0',
+    'version' => '0.14.1',
 ];

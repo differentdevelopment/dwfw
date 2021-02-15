eb0eee70caba9651eb2cbcf77c7a83146ad10456
Kukel Attila <kukel.attila@different.hu>
2021-02-15 15:57:17 +0100

    crud permission trait added

---
+++ src/app/Http/Controllers/Traits/CheckCrudPermissions.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Different\Dwfw\app\Http\Controllers\Traits;
+
+use Different\Dwfw\app\Models\Permission;
+use Illuminate\Support\Str;
+
+trait CheckCrudPermissions
+{
+    public function checkCrudPermissions()
+    {
+        abort_if('\App\Models\Entity' == $this->crud->model, 500, 'checkCrudPermissions() should be after setting crud model!');
+
+        $backpack_user = backpack_user();
+        $entity_name = Str::plural(Str::lower(class_basename($this->crud->model::class)));
+        foreach (Permission::query()->where('name', 'like', $entity_name . '.%')->get() as $permission) {
+            if (!$backpack_user->can($permission->name)) $this->crud->denyAccess(Str::replaceFirst($entity_name . '.', '', $permission->name));
+        }
+    }
+
+    public function checkCrudPermissionsByPermission($permission)
+    {
+        abort_if('\App\Models\Entity' == $this->crud->model, 500, 'checkCrudPermissions() should be after setting crud model!');
+
+        $entity_name = Str::of(class_basename($this->crud->model::class))->snake()->lower()->plural();
+        if (backpack_user()->cannot($entity_name . '.' . $permission)) {
+            $this->crud->denyAccess(['list', 'create', 'update', 'delete', 'show']);
+        }
+    }
+}

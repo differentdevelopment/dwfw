bd8a66291658de68c3890d707a7dfb394c654d02
Hoagie <hoagie@different.hu>
2021-03-11 12:22:51 +0100

    https://bug.different.hu/view.php?id=7522 automata érzékelés, hogy mely tábláknak van account_idje és melyeknek nincs

--- src/app/Scopes/AccountScope.php
+++ src/app/Scopes/AccountScope.php
@@ -5,6 +5,7 @@ namespace Different\Dwfw\app\Scopes;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Scope;
+use Illuminate\Support\Facades\Schema;
 
 class AccountScope implements Scope
 {
@@ -17,6 +18,11 @@ class AccountScope implements Scope
      */
     public function apply(Builder $builder, Model $model)
     {
+        // ezt felvenném a modellbe, hogy ne így kelljen mindig csekkolni, hogy ez egy accountos tábla
+        // vagy egy közös ősmodell, vagy pedig valami azonnal kikérdezhető érték: hasAccount = true
+        if (!Schema::hasColumn($model->getTable(), 'account_id')) {
+            return;
+        }
         if (session('account_id') != -1) {
             $builder->whereAccountId(session('account_id'));
         } else {

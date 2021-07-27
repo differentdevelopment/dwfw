b89b94bd364032b4f19a9d35b66f54c19b5f63ba
Kukel Attila <kukel.attila@different.hu>
2021-07-27 07:50:55 +0200

    tabok jogosultság kezelése, helper függvények

--- src/app/Helpers/helpers.php
+++ src/app/Helpers/helpers.php
@@ -1,8 +1,83 @@
 <?php
 
-function log_db()
-{
-    \Illuminate\Support\Facades\DB::listen(function ($query) {
-        logger($query->sql, $query->bindings);
-    });
+if (!function_exists('log_db')) {
+    function log_db()
+    {
+        \Illuminate\Support\Facades\DB::listen(function ($query) {
+            logger($query->sql, $query->bindings);
+        });
+    }
+}
+
+
+if (!function_exists('remember_with_lock')) {
+    function remember_with_lock($key, $callback, $lock_time = 10, $ttl = null)
+    {
+        return \App\Helpers\Facades\CacheWithLock::remember($key, $callback, $lock_time, $ttl);
+    }
+}
+
+if (!function_exists('dd500')) {
+    function dd500(...$vars)
+    {
+        foreach ($vars as $v) {
+            http_response_code(500);
+            \Symfony\Component\VarDumper\VarDumper::dump($v);
+        }
+        exit(1);
+    }
+}
+
+if (!function_exists('english_alphabet_only')) {
+    function english_alphabet_only(string $string)
+    {
+        return str_replace(['ű', 'ü', 'ú', 'ó', 'á', 'é', 'í', 'ö', 'ő'], ['u', 'u', 'u', 'o', 'a', 'e', 'i', 'o', 'o'], $string);
+    }
+}
+
+if (!function_exists('dump500')) {
+    function dump500($var, ...$moreVars)
+    {
+        http_response_code(500);
+        \Symfony\Component\VarDumper\VarDumper::dump($var);
+
+        foreach ($moreVars as $v) {
+            http_response_code(500);
+            \Symfony\Component\VarDumper\VarDumper::dump($v);
+        }
+
+        if (1 < func_num_args()) {
+            return func_get_args();
+        }
+
+        return $var;
+    }
+}
+
+if (!function_exists('user_can')) {
+    function user_can($permission)
+    {
+        return Auth()->user()->can($permission);
+    }
+}
+
+if (!function_exists('user_superadmin')) {
+    function user_superadmin()
+    {
+        return Auth()->user()->hasRole('super admin');
+    }
+}
+
+if (!function_exists('abort_unless_can')) {
+    function abort_unless_can($permission, $http_status_code = 403)
+    {
+        abort_unless(user_can($permission), $http_status_code);
+    }
+}
+
+if (!function_exists('abort_unless_super_admin')) {
+    function abort_unless_super_admin($http_status_code = 403)
+    {
+        abort_unless(user_superadmin(), $http_status_code);
+    }
 }
--- src/resources/views/partials/tabs.blade.php
+++ src/resources/views/partials/tabs.blade.php
@@ -2,14 +2,16 @@
     <div class="{{ isset($crud) ? $crud->getCreateContentClass() : 'col-12' }}">
         <ul class="nav nav-tabs">
             @foreach ($tabs as $tab)
-                <li class="nav-item">
-                    <a
-                        class="nav-link {{ request()->routeIs($tab['route']) ? ' active' : '' }} {{ isset($tab['only_with_entry']) && $tab['only_with_entry'] && !isset(${$tab['entry_name'] ?? 'entry'}) ? ' disabled ' : '' }}"
-                        href="{{ isset(${$tab['entry_name'] ?? 'entry'}) ? route($tab['route'], ${$tab['entry_name'] ?? 'entry'}) : '#'}}"
-                    >
-                        {{ $tab['label'] }}
-                    </a>
-                </li>
+                @if (!isset($tab['permission']) || user_can($tab['permission']))
+                    <li class="nav-item">
+                        <a
+                            class="nav-link {{ request()->routeIs($tab['route']) ? ' active' : '' }} {{ isset($tab['only_with_entry']) && $tab['only_with_entry'] && !isset(${$tab['entry_name'] ?? 'entry'}) ? ' disabled ' : '' }}"
+                            href="{{ isset(${$tab['entry_name'] ?? 'entry'}) ? route($tab['route'], ${$tab['entry_name'] ?? 'entry'}) : '#'}}"
+                        >
+                            {{ $tab['label'] }}
+                        </a>
+                    </li>
+                @endif
             @endforeach
         </ul>
     </div>

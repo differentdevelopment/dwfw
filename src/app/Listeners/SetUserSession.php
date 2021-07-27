c4d720cb9771dbf3d2a94db75700b8cb809d8ea2
Kukel Attila <kukel.attila@different.hu>
2021-07-27 11:30:45 +0200

    kivezetésre kerül a Minden account egyszerre nézése, több gondot okoz, mint haszna van

--- src/app/Listeners/SetUserSession.php
+++ src/app/Listeners/SetUserSession.php
@@ -18,10 +18,11 @@ class SetUserSession
         //Gate::before super admin not working here, if someone knows the solution, pls develop it. - Urudin
         $accounts = ($user->hasPermissionTo('change account') || $user->hasRole('super admin') ? Account::all() : Auth()->user()->accounts)->pluck('name', 'id')->toArray();
         $account_id = array_key_first($accounts);
-        if (count($accounts) > 1) {
-            $accounts = ['-1' => 'Mind'] + $accounts;
+        // @deprecated kivezetésre kerül a Minden account egyszerre nézése, több gondot okoz, mint haszna van
+//        if (count($accounts) > 1) {
+//            $accounts = ['-1' => 'Mind'] + $accounts;
 //            if (Auth()->user()->hasRole('dispatcher')) session(['account_id' => -1]);
-        }
+//        }
         session(['accounts' => $accounts]);
         session(['account_ids' => array_keys($accounts)]);
         if (!session('account_id')) session(['account_id' => $account_id]);

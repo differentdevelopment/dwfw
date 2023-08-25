628d1113c27031ea92b40a106bd6ee7b8ffeca30
Kálóczi Kevin <kaloczi.kvn@gmail.com>
2023-08-25 11:10:05 +0200

    Update tabs.blade.php

--- src/resources/views/partials/tabs.blade.php
+++ src/resources/views/partials/tabs.blade.php
@@ -1,18 +1,16 @@
-<div class="row">
-    <div class="{{ isset($crud) ? $crud->getCreateContentClass() : 'col-12' }}">
-        <ul class="nav nav-tabs">
-            @foreach ($tabs as $tab)
-                @if (!isset($tab['permission']) || user_can($tab['permission']))
-                    <li class="nav-item">
-                        <a
-                            class="nav-link {{ request()->routeIs($tab['route']) ? ' active' : '' }} {{ isset($tab['only_with_entry']) && $tab['only_with_entry'] && !isset(${$tab['entry_name'] ?? 'entry'}) ? ' disabled ' : '' }}"
-                            href="{{ isset(${$tab['entry_name'] ?? 'entry'}) ? route($tab['route'], ${$tab['entry_name'] ?? 'entry'}) : '#'}}"
-                        >
-                            {{ $tab['label'] }}
-                        </a>
-                    </li>
-                @endif
-            @endforeach
-        </ul>
-    </div>
+<div class="w-100 mb-3">
+    <ul class="nav nav-pills">
+        @foreach ($tabs as $tab)
+            @if (!isset($tab['permission']) || user_can($tab['permission']))
+                <li class="nav-item">
+                    <a
+                        class="nav-link {{ request()->routeIs($tab['route']) ? ' active' : '' }} {{ isset($tab['only_with_entry']) && $tab['only_with_entry'] && !isset(${$tab['entry_name'] ?? 'entry'}) ? ' disabled ' : '' }}"
+                        href="{{ isset(${$tab['entry_name'] ?? 'entry'}) ? route($tab['route'], ${$tab['entry_name'] ?? 'entry'}) : '#'}}"
+                    >
+                        {{ $tab['label'] }}
+                    </a>
+                </li>
+            @endif
+        @endforeach
+    </ul>
 </div>
